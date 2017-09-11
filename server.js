aed8cd811eb8c84e4bae879490c5f521703cb021
John Silkey <jpsilkjr86@gmail.com>
2017-09-11 06:51:18 -0700

    incorporated webpack-dev-middleware and webpack-hot-middleware for development mode, enabling live server rendering with live webpack compiling

--- server.js
+++ server.js
@@ -9,15 +9,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // ================ Mongoose Configuration ================
-// configure mongoose promises to ES6 Promises
+// configures mongoose promises to ES6 Promises
 mongoose.Promise = Promise;
-// set database configuration
+// sets database configuration
 const remoteUri = 'mongodb://heroku_6zgtwlpt:2u5oir8pm0sp4qvkrgvmsrkb7c@ds129394.mlab.com:29394/heroku_6zgtwlpt';
 mongoose.connect(remoteUri, { useMongoClient: true });
+// for setting up local database connection (make sure to create 'nyt-react' 
+// database first on your mongoDB before running this)
 // const localUri = 'mongodb://localhost/nyt-react';
 // mongoose.connect(localUri, { useMongoClient: true });
 
-// save connection as variable
+// saves connection as variable
 const db = mongoose.connection;
 
 // ================ Express Configuration ================
@@ -30,6 +32,35 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
 // serves public directory as static, enabling html pages to link with their assets
 app.use(express.static('public'));
+
+// ============ Webpack Middleware Configurations (Development Only) ============
+// first checks to make sure NODE_ENV is in development mode (ie not production mode)
+if (process.env.NODE_ENV !== 'production') {
+  console.log('NODE_ENV is in development mode.'
+  	+ '\nConfiguring webpack-dev-middleware and webpack-hot-middleware...');
+
+  // dependendies for webpack middleware
+  const webpack = require('webpack'),
+  	webpackDevMiddleware = require('webpack-dev-middleware'),
+  	webpackHotMiddleware = require('webpack-hot-middleware'),
+  	config = require('./webpack.dev.js');
+  const compiler = webpack(config);
+
+  // webpack-dev-middleware emits files compiled by webpack to a live server.
+  // webpack-hot-middleware allows hot reload of webpack with express server (just refresh page).
+  // more info about webpack-dev-middleware at:
+  // https://webpack.js.org/guides/development/#using-webpack-dev-middleware
+  // https://github.com/webpack/webpack-dev-middleware
+  // for info on integrating hot-middleware and dev-middleware see section "Server" at:
+  // https://ditrospecta.com/javascript/react/es6/webpack/heroku/2015/08/08/deploying-react-webpack-heroku.html
+
+  // configures webpack middlewares in development mode
+  app.use(webpackHotMiddleware(compiler))
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+}
 
 // ================ Connection Establishment ================
 // show any mongoose connection errors
